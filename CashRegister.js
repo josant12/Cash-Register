7e724fc6d0e3a85e08b52b017f2771cc2d7882f2
Jose Antonio <99999793+josant12@users.noreply.github.com>
2022-03-07 02:14:09 -0600

    Update CashRegister.js

--- CashRegister.js
+++ CashRegister.js
@@ -1,51 +1,62 @@
 const currencyUnit = {
-  "PENNY": 1,
-  "NICKEL": 5,
-  "DIME": 10,
-  "QUARTER": 25,
-  "ONE": 100,
-  "FIVE": 500,
-  "TEN": 1000,
-  "TWENTY": 2000,
-  "ONE HUNDRED": 10000
-}
-
-
-function checkCashRegister(price, cash, cid) {
-
-  let changeSum = cash * 100 - price * 100;
-  let changeSumCheck = changeSum;
-  let change = [];
-  let status = '';
-
-  let cidSum = 0;
-  let filteredCid = cid.filter(elem => elem[1] !== 0).reverse();
-
-  filteredCid.forEach(elem => {
-    let curr = elem[0];
-    let currSum = elem[1] * 100;
-    cidSum += currSum;
-    let amount = 0;
-    while (changeSum >= currencyUnit[curr] && currSum > 0) {
-      amount += currencyUnit[curr];
-      changeSum -= currencyUnit[curr];
-      currSum -= currencyUnit[curr];
-    }
-    if (amount !== 0) {
-      change.push([curr, amount / 100]);
-    }
-  });
-
-  if (changeSum > 0) {
-    status = 'INSUFFICIENT_FUNDS';
-    change = [];
-  } else if (changeSum == 0 && changeSumCheck == cidSum) {
-    status = 'CLOSED';
-    change = cid;
-  } else {
-    status = 'OPEN';
+    "PENNY": 1,
+    "NICKEL": 5,
+    "DIME": 10,
+    "QUARTER": 25,
+    "ONE": 100,
+    "FIVE": 500,
+    "TEN": 1000,
+    "TWENTY": 2000,
+    "ONE HUNDRED": 10000
   }
-  return { 'status': status, 'change': change };
-}
+  
+  
+  function checkCashRegister(price, cash, cid) {
+  
+    let changeSum = cash * 100 - price * 100;
+    let changeSumCheck = changeSum;
+    let change = [];
+    let status = '';
+  
+    let cidSum = 0;
+    let filteredCid = cid.filter(elem => elem[1] !== 0).reverse();
+  
+    filteredCid.forEach(elem => {
+      let curr = elem[0];
+      let currSum = elem[1] * 100;
+      cidSum += currSum;
+      let amount = 0;
+      while (changeSum >= currencyUnit[curr] && currSum > 0) {
+        amount += currencyUnit[curr];
+        changeSum -= currencyUnit[curr];
+        currSum -= currencyUnit[curr];
+      }
+      if (amount !== 0) {
+        change.push([curr, amount / 100]);
+      }
+    });
+  
+    if (changeSum > 0) {
+      status = 'INSUFFICIENT_FUNDS';
+      change = [];
+    } else if (changeSum == 0 && changeSumCheck == cidSum) {
+      status = 'CLOSED';
+      change = cid;
+    } else {
+      status = 'OPEN';
+    }
+    return { 'status': status, 'change': change };
+  }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+  function cajaChecker(){
+    let inputP = document.getElementById("inputP");
+    let inputCa = document.getElementById("inputCa");
+    let inputCi = document.getElementById("inputCi");  
+    inputCi = inputCi.value;
+    let inputCi2 =  JSON.parse(inputCi);  
+    inputP = inputP.value;
+    inputCa = inputCa.value;
+    let check = checkCashRegister(inputP, inputCa, inputCi2);
+    check = JSON.stringify(check);
+    alert(check);
+  }
